a741a7cbfc2e9e0d964b882bdae53852ab92c773
mhamzahhanif <mhamzah1993@gmail.com>
2018-11-17 14:09:17 -0500

    initial

---
+++ routes/apiRoutes.js
@@ -0,0 +1,11 @@
+var db = require ("../models");
+var axios = require ("axios");
+
+module.exports = function(app) {
+    app.get("/api/movies", function(req, res){
+      var title = req.query.search;
+      // "/api/movies?search=movietitle"
+      axios.get("https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy").then(function(json) {
+        res.status(200).json(json);
+      });
+    });
