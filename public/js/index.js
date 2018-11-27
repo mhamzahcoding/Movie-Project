644f20a94a3b2d7ae6618bb2483c8ad754ec4c14
Nico Almena <fnalmenaradumur96@gmail.com>
2018-11-27 18:45:36 -0500

    Search is now working and up to date with hamzah's branch

--- public/js/index.js
+++ public/js/index.js
@@ -1,84 +1,57 @@
-$(document).ready(() => {
-    $('#searchForm').on('submit', (e) => {
-      let searchText = $('#searchText').val();
-      getMovies(searchText);
-      e.preventDefault();
-    });
+/* global moment */
+
+// When the page loads, grab and display all of our chirps
+$.get("/api/all", function(data) {
+
+    if (data.length !== 0) {
+  
+      for (var i = 0; i < data.length; i++) {
+  
+        var row = $("<div>");
+        row.addClass("chirp");
+  
+        row.append("<p>" + data[i].name + " reviewd.. </p>");
+        row.append("<p>" + data[i].body + "</p>");
+        row.append("<p>At " + moment(data[i].created_at).format("h:mma on dddd") + "</p>");
+  
+        $("#review-area").prepend(row);
+  
+      }
+  
+    }
+  
   });
   
-  function getMovies(searchText){
-    axios.get('http://www.omdbapi.com?s='+ searchText + "&y=&plot=short&apikey=trilogy")
-      .then((response) => {
-        console.log(response);
-        let movies = response.data.Search;
-        let output = '';
-        $.each(movies, (index, movie) => {
-          output += `
-            <div class="col-md-3">
-              <div class="well text-center">
-                <img src="${movie.Poster}">
-                <h5>${movie.Title}</h5>
-                <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="/movie.html">Movie Details</a>
-              </div>
-            </div>
-          `;
-        });
+  // When user chirps (clicks addBtn)
+  $("#review-submit").on("click", function(event) {
+    event.preventDefault();
   
-        $('#movies').html(output);
-      })
-      .catch((err) => {
-        console.log(err);
+    // Make a newChirp object
+    var newChirp = {
+      author: $("#name-box").val().trim(),
+      body: $("#review-box").val().trim(),
+      created_at: moment().format("YYYY-MM-DD HH:mm:ss")
+    };
+  
+    console.log(newChirp);
+  
+    // Send an AJAX POST-request with jQuery
+    $.post("/api/new", newChirp)
+      // On success, run the following code
+      .then(function() {
+  
+        var row = $("<div>");
+        row.addClass("chirp");
+  
+        row.append("<p>" + newChirp.author + " chirped: </p>");
+        row.append("<p>" + newChirp.body + "</p>");
+        row.append("<p>At " + moment(newChirp.created_at).format("h:mma on dddd") + "</p>");
+  
+        $("#review-area").prepend(row);
+  
       });
-  }
   
-  function movieSelected(id){
-    sessionStorage.setItem('movieId', id);
-    window.location = 'movie.html';
-    return false;
-  }
-  
-  function getMovie(){
-    let movieId = sessionStorage.getItem('movieId');
-    console.log(movieId);
-  
-    axios.get('http://www.omdbapi.com?i='+movieId + "&y=&plot=short&apikey=trilogy")
-      .then((response) => {
-        console.log(response);
-        let movie = response.data;
-  
-        let output =`
-          <div class="row">
-            <div class="col-md-4">
-              <img src="${movie.Poster}" class="thumbnail">
-            </div>
-            <div class="col-md-8">
-              <h2>${movie.Title}</h2>
-              <ul class="list-group">
-                <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
-                <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
-                <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
-                <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
-                <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
-                <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
-                <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
-              </ul>
-            </div>
-          </div>
-          <div class="row">
-            <div class="well">
-              <h3>Plot</h3>
-              ${movie.Plot}
-              <hr>
-              <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
-              <a href="index.html" class="btn btn-default">Go Back To Search</a>
-            </div>
-          </div>
-        `;
-  
-        $('#movie').html(output);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-  
+    // Empty each input box by replacing the value with an empty string
+    $("#name-box").val("");
+    $("#review-box").val("");
+  });
